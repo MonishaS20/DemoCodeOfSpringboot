bcbebef31dc300f0d7f61b3de2f7ec4d7cbaec9a
MonishaS20 <114240839+MonishaS20@users.noreply.github.com>
2022-09-23 21:03:06 +0530

    Initial commit

---
+++ demo/src/main/java/com/example/demo/DemoProjectController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller 
+public class DemoProjectController {
+	 @GetMapping("/welcome")
+	 @ResponseBody
+	 
+	 public String welcome() {
+		 
+		 return "<h1>Hello Monisha<h1>";
+	 }
+	 
+
+}
